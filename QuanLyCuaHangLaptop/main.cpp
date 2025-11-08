0024059ace21d578926467a31b0dc49ba193a2b0
huuthang9764 <huuthang9764@gmail.com>
2025-11-08 15:42:54 +0700

    Add project files.

---
+++ QuanLyCuaHangLaptop/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <conio.h> 
+#include "thuvien.h" 
+#include "xuly.h"   
+
+
+using namespace std;
+
+// =================== KHAI BÁO BIẾN TOÀN CỤC ===================
+DanhSachLaptop        g_dsLaptop;
+DanhSachKhachHang     g_dsKhachHang;
+DanhSachHoaDon        g_dsHoaDonToanCuc;
+DanhSachNhanVien      g_dsNhanVien;
+DanhSachTaiKhoan      g_dsTaiKhoan;
+DanhSachThuongHieu    g_dsThuongHieu;
+DanhSachLoai          g_dsLoai;
+DanhSachBaoHanh       g_dsBaoHanh;
+
+int main() {
+    
+
+    return 0;
+}
+
---
+++ QuanLyCuaHangLaptop/xuly.cpp
@@ -0,0 +1,35 @@
+﻿#include "xuly.h"
+#include <iostream>
+#include <fstream>  // Thư viện để Đọc/Ghi file
+#include <cstring>
+
+using namespace std;
+
+// =================== 1. ĐỌC / GHI FILE ===================
+
+void DocDuLieu(
+    DanhSachKhachHang& dskh,
+    DanhSachHoaDon& dsHD_ToanCuc,
+    DanhSachLaptop& dsls,
+    DanhSachNhanVien& dsnv,
+    DanhSachTaiKhoan& dstk,
+    DanhSachThuongHieu& dsth,
+    DanhSachLoai& dsl,
+    DanhSachBaoHanh& dsbh){}
+
+void GhiDuLieu(
+    const DanhSachKhachHang& dskh,
+    const DanhSachHoaDon& dsHD_ToanCuc,
+    const DanhSachLaptop& dsls,
+    const DanhSachNhanVien& dsnv,
+    const DanhSachTaiKhoan& dstk,
+    const DanhSachThuongHieu& dsth,
+    const DanhSachLoai& dsl,
+    const DanhSachBaoHanh& dsbh)
+{
+}
+    
+
+void DongBoHoaDonLong(DanhSachKhachHang& dskh, const DanhSachHoaDon& dsHD_ToanCuc) {
+   
+}
